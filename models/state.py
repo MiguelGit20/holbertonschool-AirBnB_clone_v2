369589fd8a440cb2cc2edd4bdac6799ce007d481
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-04 22:08:56 -0500

    update

--- models/state.py
+++ models/state.py
@@ -13,17 +13,11 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state', cascade='delete')
-
-    else:
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             from models import storage
-            """Returns the list of City instances"""
-            lst = []
-            for c in list(storage.all(City).values()):
-                if City.state_id == self.id:
-                    lst = lst.append(c)
-            return lst
+            return [c for c in list(
+                storage.all(City).values()) if c.state_id == self.id]
