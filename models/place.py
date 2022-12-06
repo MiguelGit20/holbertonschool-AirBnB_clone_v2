b8d040873f74145e1e17d9f62018add81757bf96
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-06 14:21:52 -0500

    .

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Float
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    
+
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
