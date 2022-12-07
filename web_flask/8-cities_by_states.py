e5be8aafbdcfe25815cd6144efdee181e6d416dc
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-07 12:48:56 -0500

    Cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+"""
+from flask import Flask
+from markupsafe import escape
+from flask import render_template
+from models import storage
+from models.state import State
+from os import getenv
+from sqlalchemy.orm import relationship
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    from models.state import State
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        obj = State()
+        relation_cities = obj.cities
+        return relation_cities
+    else:
+        cities_obj = State.cities()
+        states_obj = [s for s in storage.all("State").values()]
+        return render_template("8-cities_by_states.html", states_obj=states_obj, cities_obj=cities_obj)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
